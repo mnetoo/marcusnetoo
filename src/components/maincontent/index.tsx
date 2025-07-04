3b2dba953c144d030e0664ea2d7b9e37c942fd20
Marcus Neto <marcusnetoo@outlook.com>
2025-07-04 19:26:50 -0300

    ajuste download

--- src/components/maincontent/index.tsx
+++ src/components/maincontent/index.tsx
@@ -9,18 +9,10 @@ const Hero = () => {
 
         <h1>Marcus Neto</h1>
         <p>Biomedical Informatic & Back-end Developer</p>
-
         <div className="cta-buttons">
-          <button className="button primary-button"
-            onClick={() => {
-              const link = document.createElement('a');
-              link.href = curriculo;
-              link.download = 'Curriculo.pdf';
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-            }}>Download CV 
-          </button>
+          <a href={curriculo} download="Curriculo.pdf">
+            <button className="button primary-button">Download CV</button>
+          </a>
         </div>
       </div>
       <div className="hero-right">
